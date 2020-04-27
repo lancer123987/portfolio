abcc07af857650702fb78ce2e61b86a67cd6ced0
Lancer <lancer123987@gmail.com>
2020-04-28 03:02:40 +0800

    04280302

--- js/style.js
+++ js/style.js
@@ -1,6 +1,15 @@
-$(document).ready(function() {
-	var Today=new Date();
-	var age=Today.getFullYear() - 1994;
+var loading = function () {
+	$('.line.top').addClass('leaveT');
+	$('.line.bottom').addClass('leaveB');
+	$('.positionZero').css('animation-name', 'none');
+	$('.positionZero').delay(3500).fadeOut(500);
+};
+
+$(document).ready(function () {
+	setTimeout(function () { loading(); }, 4000);
+
+	var Today = new Date();
+	var age = Today.getFullYear() - 1994;
 	$("#copyright").html("<p>Copyright &copy;" + Today.getFullYear() + " Lancer All Rights Reserved.</p>");
 	$("#age").html('年齡：' + age);
 });
