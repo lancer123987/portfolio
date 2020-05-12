7d220f9abc851e5602c4ebe232b0fa22972212ad
Lancer <lancer123987@gmail.com>
2020-05-12 15:49:00 +0800

    0512

--- js/style.js
+++ js/style.js
@@ -20,6 +20,8 @@ $(document).ready(function () {
 
 	$('.workArea').slick({
 		dots: true,
+		prevArrow: '<button type="button" class="slick-prev">◄</button>',
+		nextArrow: '<button type="button" class="slick-next">►</button>',
 		responsive: [
 			{
 				breakpoint: 768,
