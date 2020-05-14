e27b428808e302e50dc375aafb39407e30030336
Lancer <lancer123987@gmail.com>
2020-05-15 02:55:27 +0800

    Update style.js

--- js/style.js
+++ js/style.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
 	setTimeout(function () { loading(); }, 4000);
 
 	var Today = new Date();
-	var age = Today.getFullYear() - 1994;
+	var age = Today.getFullYear() - 1994 - 1;
 	$("#copyright").html("<p>Copyright &copy;" + Today.getFullYear() + " Lancer All Rights Reserved.</p>");
 	$("#age").html('年齡：' + age);
 
